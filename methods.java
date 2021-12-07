cea34061e2de2553ec92361067e264735b2f74a6
SushanGhimire <sushangmi55@gmail.com>
2021-12-07 08:49:56 +0545

    methods added

---
+++ methods.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PracticalJava7thSem;
+
+/**
+ *
+ * @author sushan
+ */
+public class methods {
+    public void callMe(){
+        System.out.println("I am being called.....");
+    }
+    public static void main(String args[]){
+        methods m= new methods();
+        m.callMe();
+    }
+}
