c500b4cd823b730deb1bcba1f690a05d93f39d7d
SushanGhimire <sushangmi55@gmail.com>
2021-12-07 19:02:22 +0545

    static keyword added

---
+++ Static_keyword.java
@@ -0,0 +1,12 @@
+
+package PracticalJava7thSem;
+
+public class Static_keyword {
+    static String name="Sushan";
+    static int age=24;
+    public static void main(String args[]){
+        System.out.println(name);
+        System.out.println(age);
+
+    }
+}
