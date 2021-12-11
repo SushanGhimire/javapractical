8fb8ba4af876b009ea75f5be651d5cc5c05ef111
SushanGhimire <sushangmi55@gmail.com>
2021-12-11 09:27:43 +0545

    vowel and cosonent added

---
+++ fileHandeling/Vowel.java
@@ -0,0 +1,28 @@
+
+package PracticalJava7thSem.fileHandeling;
+import java.util.Scanner;
+import java.io.*;
+import java.io.IOException;
+public class Vowel {
+    public static void main(String args[]) throws IOException{
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the character you want");
+        String s= input.nextLine();
+        
+        FileWriter vol= new FileWriter("Vowel.txt");
+        FileWriter con= new FileWriter("consonant.txt");
+        
+        for(int i=0; i<s.length();i++){
+            char c= s.charAt(i);
+            if(c=='a' || c=='e' ||c=='i' ||c=='o' ||c=='u'){
+                vol.write(c);
+                vol.flush();
+            }else{
+                con.write(c);
+                con.flush();
+            }
+        }
+        vol.close();
+        con.close();
+    }
+}
