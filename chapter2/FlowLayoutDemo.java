0a7380c32078b6f0bd25c9ee5f018077b2762c77
SushanGhimire <sushangmi55@gmail.com>
2021-12-14 22:16:01 +0545

    flow layout updated

--- chapter2/FlowLayoutDemo.java
+++ chapter2/FlowLayoutDemo.java
@@ -2,9 +2,9 @@
 package PracticalJava7thSem.chapter2;
 
 import java.awt.*;
-public class FlowLayout extends Frame {
+public class FlowLayoutDemo extends Frame {
     Button b1,b2,b3;
-    FlowLayout(){
+    FlowLayoutDemo(){
         setSize(400,150);
         setTitle("Flowlayout Demo");
         b1 = new Button("Button 1");
@@ -13,9 +13,10 @@ public class FlowLayout extends Frame {
         add(b1);
         add(b2);
         add(b3);
+        setLayout(new FlowLayout());    
         setVisible(true);
     }
     public static void main(String args[]){
-        new FlowLayout();
+        new FlowLayoutDemo();
     }
 }
